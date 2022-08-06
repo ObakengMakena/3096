55ea6dabf80239cef758744a023f77221e7c90f8
ObakengMakena <mknoba001@myuct.ac.za>
2022-08-07 01:00:33 +0200

    upload code

---
+++ 3096-Pracs-MKNOBA001/Prac1/main.c
@@ -0,0 +1,15 @@
+# include <stdio.h>
+int main(){
+
+	int a,b,sum;
+		
+	printf("enter a value for a: ");
+	scanf("%d", &a);
+	
+	printf("enter a value for b: ");
+	scanf("%d", &b);
+	
+	sum=a+b;	
+	
+	printf("the sum of a and b is %d \n.", sum);
+}
